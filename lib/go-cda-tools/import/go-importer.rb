ae5168671ab6556c876e4d3b3c223b860aadf26e
Andrew Hubley <ahubley@mitre.org>
2016-06-24 14:20:48 -0400

    added binary shared object and header file, minor changes to import and test

--- lib/go-cda-tools/import/go-importer.rb
+++ lib/go-cda-tools/import/go-importer.rb
@@ -12,6 +12,7 @@ module GoCDATools
 
         def parse_with_ffi(file)
           data = file.kind_of?(String) ? file : file.inner_html
+          data.gsub!("<?xml-stylesheet type=\"text/xsl\" href=\"cda.xsl\">",'')
           patient_json_string = import_cat1(data)
           patient = Record.new(JSON.parse(patient_json_string))
           patient
